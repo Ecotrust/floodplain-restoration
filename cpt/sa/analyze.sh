ab1d025961017f683b80bfbb5ef1870f84c423b4
Matthew Perry <perrygeo@gmail.com>
2014-04-25 13:53:31 -0700

    sensitivity and posterior distribution

---
+++ cpt/sa/analyze.sh
@@ -0,0 +1,5 @@
+python -B -m SALib.analyze \
+       -m fast \
+       -p params.txt \
+       -Y results.txt \
+       -c 0 \
---
+++ cpt/sa/sample.sh
@@ -0,0 +1,7 @@
+python -B -m SALib.sample \
+       -m fast \
+       -n 100 \
+       -p params.txt \
+       -o samples.txt \
+       --delimiter=' ' \
+       --precision=2 \
