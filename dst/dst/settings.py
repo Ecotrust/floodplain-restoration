20e152d9d8ff2bef164ea6732a91515b7db218ea
rhodges <rhodges@ecotrust.org>
2014-10-15 13:44:14 -0700

    adding styled login and logout pages

--- dst/dst/settings.py
+++ dst/dst/settings.py
@@ -27,6 +27,15 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, 'dst/templates'))
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader'
+)
+
 
 # Application definition
 
