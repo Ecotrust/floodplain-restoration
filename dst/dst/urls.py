5c3942593762a9321cc7f56fb13ea3335f8d615a
rhodges <rhodges@ecotrust.org>
2014-10-21 17:35:44 -0700

    adding style to account header to match main site

--- dst/dst/urls.py
+++ dst/dst/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from survey import views
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -11,6 +12,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^silk/', include('silk.urls', namespace='silk')),
-    url(r'^sessions/auth.js$', views.auth)
-
+    url(r'^sessions/auth.js$', views.auth),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True})
 )
