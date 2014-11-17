075b33cfa2197c628b07f371128ba8cd573f5dc2
rhodges <rhodges@ecotrust.org>
2014-11-17 14:38:14 -0800

    displaying suitability scores with d3

--- ui/app/scripts/controllers/surveydone.js
+++ ui/app/scripts/controllers/surveydone.js
@@ -9,6 +9,7 @@
  */
 if(false) {
   var map=null;
+  var d3 = null;
 }
 
 angular.module('uiApp')
@@ -27,6 +28,84 @@ angular.module('uiApp')
       .getSuitabilityScores($routeParams.siteId)
       .then( function() {
         $rootScope.suitability = SiteFactory.suitability;
+        var suitabilityList= [];
+        var suitabilityCategories = {
+          'low': {
+            'minScore' : 0,
+            'maxScore' : 33,
+            'label' : 'Unsuitable',
+            'bgColor': 'gray'
+          },
+          'med' : {
+            'minScore': 33,
+            'maxScore': 66,
+            'label': 'Moderately Suitable',
+            'bgColor': 'yellow'
+          },
+          'high' : {
+            'minScore': 66,
+            'maxScore': 100,
+            'label': 'Highly Suitable',
+            'bgColor': 'green'
+          }
+        };
+
+        var suitabilityScoreTypes = {
+          'site': 'Site',
+          'socio_economic': 'Socio-Economic',
+          'landscape': 'Landscape',
+          'suitability': 'Overall'
+        };
+
+        for (var key in suitabilityScoreTypes) {  //TODO: what if keys do not match?
+          suitabilityList.push({
+            'key': key,
+            'value': Math.floor(SiteFactory.suitability[key] * 100)
+          });
+        }
+
+        var scoreDivs = d3.select('.suitability-scores')
+          .selectAll('div')
+            .data(suitabilityList)
+            .enter().append('div')
+              .attr('class', 'suitability-score');
+
+        var scoreDivPs = scoreDivs.append('p');
+
+        scoreDivPs.append('span')
+          .attr('class', 'suitability-label')
+          .text(function(d) {return suitabilityScoreTypes[d.key] + ': ';});
+            
+        scoreDivPs.append('span')
+          .attr('class', 'suitability-category')
+          .text(function(d) {
+            for (var catKey in suitabilityCategories) {
+              var cat = suitabilityCategories[catKey];
+              if (d.value <= cat.maxScore && d.value >= cat.minScore) {
+                console.log(cat.label);
+                return cat.label;
+              }
+            }
+            console.log('no label for ' + d.key);
+            return null;
+          });
+
+        scoreDivs.append('div')
+          .attr('class', 'suitability-bar')
+          .style('width', function(d) { return d.value + '%';})
+          .style('background-color', function(d) {
+            for (var catKey in suitabilityCategories) {
+              var cat = suitabilityCategories[catKey];
+              if (d.value <= cat.maxScore && d.value >= cat.minScore) {
+                console.log(cat.bgColor);
+                return cat.bgColor;
+              }
+            }
+            console.log('no bg color for ' + d.key);
+            return 'transparent';
+          })
+          .text(function(d) {return d.value;});
+
       });
     // $scope.questions = QuestionFactory.getQuestions();
     $scope.maxQuestionId = 2;  //QuestionFactory will likely change substantially
@@ -42,7 +121,4 @@ angular.module('uiApp')
         map.showMap(true);
       });
 
-    // map.clear();
-    // map.loadSites(SiteFactory.getActiveSiteCollection());
-    // map.loadPits(SiteFactory.getPitsCollection());
   });
