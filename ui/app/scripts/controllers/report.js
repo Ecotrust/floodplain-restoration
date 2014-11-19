b287bb60bd34dcd3d53d40e220914297e19a5b4e
rhodges <rhodges@ecotrust.org>
2014-11-19 11:50:19 -0800

    Implementation of report basics, not data yet

--- ui/app/scripts/controllers/report.js
+++ ui/app/scripts/controllers/report.js
@@ -10,7 +10,7 @@ if (false) {var map =  null;}
  * Controller of the uiApp
  */
 angular.module('uiApp')
-  .controller('ReportCtrl', function ($scope, $rootScope, $window) {
+  .controller('ReportCtrl', function ($scope, $rootScope, $window, $routeParams, SiteFactory, QuestionFactory, NodeFactory) {
 
     if (!$rootScope.userName) {
       $window.alert('You are not logged in. You will now be redirected to the login page.');
@@ -24,4 +24,140 @@ angular.module('uiApp')
       'AngularJS',
       'Karma'
     ];
+
+    $rootScope.activeSiteId = $routeParams.siteId;
+    $scope.pits = [];
+
+    SiteFactory
+      .getSites()
+      .then( function() {
+
+        $scope.sites = SiteFactory.sites.features;
+
+        // set active site
+        for (var i = SiteFactory.sites.features.length - 1; i >= 0; i--) {
+          var site = SiteFactory.sites.features[i];
+          if (site.id === parseInt($rootScope.activeSiteId, 10)) {
+            $scope.site = site;
+            $scope.pits = site.properties.pit_set;
+            break;
+          }
+        }
+
+      });
+
+    SiteFactory
+      .getSuitabilityScores($routeParams.siteId)
+      .then( function() {
+
+        $rootScope.suitability = SiteFactory.suitability;
+        var suitabilityList= [];
+        var suitabilityCategories = {
+          'low': {
+            'minScore' : 0,
+            'maxScore' : 33,
+            'label' : 'Unsuitable',
+            'bgColor': 'gray'
+          },
+          'med' : {
+            'minScore': 33,
+            'maxScore': 66,
+            'label': 'Moderately Suitable',
+            'bgColor': 'yellow'
+          },
+          'high' : {
+            'minScore': 66,
+            'maxScore': 100,
+            'label': 'Highly Suitable',
+            'bgColor': 'green'
+          }
+        };
+
+        var suitabilityScoreTypes = {
+          'site': 'Site',
+          'socio_economic': 'Socio-Economic',
+          'landscape': 'Landscape',
+          'suitability': 'Overall'
+        };
+
+        for (var key in suitabilityScoreTypes) {  //TODO: what if keys do not match?
+          suitabilityList.push({
+            'key': key,
+            'value': Math.floor(SiteFactory.suitability[key] * 100)
+          });
+        }
+
+        // var scoreDivs = d3.select('.suitability-scores')
+        //   .selectAll('div')
+        //     .data(suitabilityList)
+        //     .enter().append('div')
+        //       .attr('class', 'suitability-score');
+
+        // var scoreDivPs = scoreDivs.append('p');
+
+        // scoreDivPs.append('span')
+        //   .attr('class', 'suitability-label')
+        //   .text(function(d) {return suitabilityScoreTypes[d.key] + ': ';});
+            
+        // scoreDivPs.append('span')
+        //   .attr('class', 'suitability-context')
+        //   .text(function(d) {
+        //     for (var catKey in suitabilityCategories) {
+        //       var cat = suitabilityCategories[catKey];
+        //       if (d.value <= cat.maxScore && d.value >= cat.minScore) {
+        //         console.log(cat.label);
+        //         return cat.label;
+        //       }
+        //     }
+        //     console.log('no label for ' + d.key);
+        //     return null;
+        //   });
+
+        // scoreDivs.append('div')
+        //   .attr('class', 'suitability-bar')
+        //   .style('width', function(d) { return d.value + '%';})
+        //   .style('background-color', function(d) {
+        //     for (var catKey in suitabilityCategories) {
+        //       var cat = suitabilityCategories[catKey];
+        //       if (d.value <= cat.maxScore && d.value >= cat.minScore) {
+        //         console.log(cat.bgColor);
+        //         return cat.bgColor;
+        //       }
+        //     }
+        //     console.log('no bg color for ' + d.key);
+        //     return 'transparent';
+        //   })
+        //   .text(function(d) {return d.value;});
+
+      });
+    // $scope.questions = QuestionFactory.getQuestions();
+    $scope.maxQuestionId = 2;  //QuestionFactory will likely change substantially
+                                  //We'll hardcode this for now.
+
+    var questions = [];
+    var nodes = [];
+
+    QuestionFactory
+      .getQuestions()
+      .then( function() {
+        questions = QuestionFactory.questions;
+        $scope.numQuestions = questions.length;  //QuestionFactory will likely change substantially
+        $scope.maxQuestionId = questions[questions.length-1].id;
+
+        NodeFactory
+          .getNodes($rootScope.activeSiteId)
+          .then( function() {
+            nodes = NodeFactory.nodes;
+            $scope.numNodes = nodes.length;
+            
+            map.showMap(false);
+          });
+
+        
+        map.showMap(false);
+      });
+
+    $scope.alert = function (msg) {
+      $window.alert(msg);
+    }
   });
