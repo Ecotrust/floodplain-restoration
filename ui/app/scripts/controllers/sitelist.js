a66d5147986b56e7ea9c66a0004912285ab08f21
rhodges <rhodges@ecotrust.org>
2014-11-20 10:31:29 -0800

    making property-list boxes better reflect mockups. For #66

--- ui/app/scripts/controllers/sitelist.js
+++ ui/app/scripts/controllers/sitelist.js
@@ -10,29 +10,47 @@ if (false) { var map; }
  * Controller of the uiApp
  */
 angular.module('uiApp')
-  .controller('SitelistCtrl', function ($scope, $rootScope, $window, SiteFactory) {
+  .controller('SitelistCtrl', function ($scope, $rootScope, $window, SiteFactory, QuestionFactory, NodeFactory) {
 
     if (!$rootScope.userName) {
-      alert('You are not logged in. You will now be redirected to the login page.');
+      $window.alert('You are not logged in. You will now be redirected to the login page.');
       $window.location = '/accounts/login/';
     }
 
     map.showMap(true);
     
     $scope.sites = [];
-
     $rootScope.activeSiteId = null;
+    $scope.numQuestions = 0;
 
-    SiteFactory.getSites()
+    function getNodes(site) {
+      NodeFactory
+        .getNodes($scope.sites[site].id)
+        .then( function() {
+          $scope.sites[site].nodeCount = NodeFactory.nodes.length;
+        });
+    }
+
+    SiteFactory
+      .getSites()
       .then( function() {
         $scope.sites = SiteFactory.sites.features;
         map.clear();
         map.loadSites(SiteFactory.sites);
         map.showMap(true);
+        for (var site in $scope.sites) {
+          getNodes(site);
+        }
+      });
+
+    QuestionFactory
+      .getQuestions()
+      .then( function() {
+        $scope.numQuestions = QuestionFactory.questions.length;
       });
 
     $scope.deleteSite = function(siteId) {
-      if (confirm('Delete this property? Are you sure?') === true) {
+      if ($window.confirm('Delete this property? Are you sure?') === true) {
         SiteFactory.deleteSite(siteId).then(function() {
 
           // Update new sites
@@ -52,6 +70,6 @@ angular.module('uiApp')
       } else {
         icon.className = icon.className.replace(/\bglyphicon-minus-sign\b/,'glyphicon-plus-sign');
       }
-    }
+    };
 
   });
