622319814e8c58f09f7105e05ee9ed1e5bd358e8
rhodges <rhodges@ecotrust.org>
2014-12-05 13:59:09 -0800

    updating survey questions

--- dst/survey/admin.py
+++ dst/survey/admin.py
@@ -5,4 +5,10 @@ admin.site.register(GravelSite, admin.GeoModelAdmin)
 admin.site.register(Pit, admin.GeoModelAdmin)
 admin.site.register(InputNode, admin.ModelAdmin)
 admin.site.register(MapLayer, admin.ModelAdmin)
-admin.site.register(Question, admin.ModelAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'question', 'order', 'category')
+    list_filter = ['title', 'question', 'order', 'category']
+    ordering = ['order', 'category', 'title', 'question']
+
+admin.site.register(Question, QuestionAdmin)
