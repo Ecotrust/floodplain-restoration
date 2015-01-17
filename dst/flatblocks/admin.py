244a8faec7f3fa8fdf6cf21a5520250583d5b06d
rhodges <rhodges@ecotrust.org>
2015-01-16 16:14:11 -0800

    settling in to new flatblocks

--- dst/flatblocks/admin.py
+++ dst/flatblocks/admin.py
@@ -4,7 +4,7 @@ from flatblocks.models import FlatBlock
 
 class FlatBlockAdmin(admin.ModelAdmin):
     ordering = ['slug', ]
-    list_display = ('slug', 'header')
+    list_display = ('slug', 'header', 'content')
     search_fields = ('slug', 'header', 'content')
 
 admin.site.register(FlatBlock, FlatBlockAdmin)
