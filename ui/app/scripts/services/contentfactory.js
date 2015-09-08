01878831479db664f6ff22130891141ccf026e4a
rhodges <ryan.d.hodges@gmail.com>
2015-09-08 16:31:36 -0700

    Allowing user to edit content of confusing Summary Page title

--- ui/app/scripts/services/contentfactory.js
+++ ui/app/scripts/services/contentfactory.js
@@ -11,7 +11,7 @@ angular.module('uiApp')
   .factory('ContentFactory', function ($http) {
 
     // TODO get from REST API
-    // If you need HTML here, the view needs to contain an ng-bind-html directive 
+    // If you need HTML here, the view needs to contain an ng-bind-html directive
     // e.g. <span ng-bind-html='content.attrWithHtml'></span>
     /*jshint multistr:true */
 
@@ -24,10 +24,12 @@ angular.module('uiApp')
       'hook': 'Informed decision making and resource investment <br> for the restoration of floodplain mining sites.',
       'requirements': '<a class="homepage-link" href="#/help/documents">Before you get started</a>',
       'getStarted': 'Get Started',
+      'reportIntro': 'This is the full report of your property. Please click on any sub-category to see more information.',
       'siteListHeader': 'All Sites',
       'siteCreateDirections': 'Click anywhere on the map to start defining your general site boundary. <br/><br/> Continue clicking to complete the shape of your site. <br/><br/> When you\'re satisfied with your site outline, double-click to end drawing.<br/><br/> Name and save your site when done. You can come back and edit this at any time.',
       'siteEditDirections': 'Click and hold anywhere on the site border to grab it. <br/><br/> Drag the section of border to where it belongs.',
       'siteEditDefinitions': '<div class="definition-block blue-text"><p><span class="definition-term">Site</span>: A contiguous plot of land that encompasses one or more pits.</p><p><span class="definition-term">Pit</span>: A single gravel pit mine.</p></div>',
+      'summaryTitle': 'REPORT SUMMARY',
       'pitCreateDirections': 'Like drawing your site, click once on the map to begin drawing your pit boundaries. Click again to add additional points. Double-click to finish.',
       'pitDisclaimer': 'ONLY include pits will be part of this restoration.',
       'pitEditDirections': 'Click and hold anywhere on the pit border to grab it. Drag the section of the border to where it belongs.',
@@ -79,7 +81,7 @@ angular.module('uiApp')
             service.content[data[i].slug] = data[i].content;
           }
         });
-        
+
         promise.error(function() {
           console.log('Could not fetch content.json');
         });
